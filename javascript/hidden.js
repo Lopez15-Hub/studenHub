af4de7c5870e7d7edc7e7d833293861b821f0176
Ezequiel López <ezequiel.lopez.epet20@gmail.com>
2020-05-22 20:53:53 -0300

    git-add

---
+++ javascript/hidden.js
@@ -0,0 +1,7 @@
+let ocultar = document.getElementsByTagName('button');
+let btnOculto = document.getElementsByClassName('btn-hide');
+function ocultar () {
+    for (let i = 0; btnOculto.length; i++{
+         ocultar.toggle.list       
+    }
+}
