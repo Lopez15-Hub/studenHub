51b73b45f3a836036a81fd00299a588a23c726f5
Lopez15-Hub <65269387+Lopez15-Hub@users.noreply.github.com>
2020-09-01 13:34:22 -0300

    update script 01-09 13-26.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,14 +1,13 @@
-
 function scrollOff(){  
   let head = document.getElementById("titTab");
   let foot = document.getElementsByTagName("footer");
-if(navigator.userAgent.match(Android/i)){
+//if(navigator.userAgent.match(Android/i)){
    
     window.scrollX.getElementById("btnMen").off;
     window.scrollY.getElementById("btnMen").off;
     
     head.setAttribute("hidden");
     foot.setAttribute("hidden");
-}
+//}
 }//fin programa
 
