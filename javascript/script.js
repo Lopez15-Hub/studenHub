a2a9146e6cbe165b3dc6a0f68eaad2c332a31ef7
Lopez15-Hub <ezequiellopez5678@gmail.com>
2020-09-05 15:00:39 -0300

    05-09 15:00

--- javascript/script.js
+++ javascript/script.js
@@ -1,16 +1,3 @@
-/*function scrollOff(){  
-  let head = document.getElementById("titTab");
-  let foot = document.getElementById("foot");
-if(navigator.userAgent.match(Android)){
-   prompt 
-    window.scrollX.getElementById("btnMen").off;
-    window.scrollY.getElementById("btnMen").off;
-    
-    head.setAttribute("hidden");
-    foot.removeAttribute("hidden");
-//}
-}//fin programa
-}*/
 
 
 
