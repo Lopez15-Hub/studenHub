2f94569b3121e4e61aa81f75e76a5c430e341ca1
Ezequiel López <ezequiel.lopez.epet20@gmail.com>
2020-09-12 19:54:42 -0300

    CAMBIOS 19:54 12-09

--- javascript/hidden.js
+++ javascript/hidden.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+/*$(document).ready(function () {
 
     $('#list').hide();
     //toggle button text
@@ -9,4 +9,22 @@ $(document).ready(function () {
     $('button').click(function () {
         $('#list').toggle('fast');
     });
+});*/
+
+$(document).ready(function () {
+
+    $('#menu').hide();
+    //toggle button text
+    $('button').click(function () {
+        $('span',this).toggle();
+    });
+    //toggle list
+    $('button').click(function () {
+        $('#menu').toggle('fast');
+    });
 });
+
+function cerrar(){
+    document.getElementById("menu").style="display:none";
+}
+
--- javascript/script.js
+++ javascript/script.js
@@ -20,17 +20,24 @@ document.getElementById("prof").style="display:block;margin-left:5%";
 document.getElementById("saludo").style="display:block;margin-top:9%;margin-left:10%";
 document.getElementById("state").style="margin-top:10%;margin-left:34%;display:inline-flex";
 
-
-function abrir()
+function menOff(){
+}
+function abrirMen()
  
 {
-  if(document.getElementById("btnMen").onclick)
+ var menu = document.getElementById("menu").style="display:block";
+ document.getElementById("log").style="margin-left:auto";
+ menu.addEventListener("click",abrirMen,true);
+
+}
+
+
+
+
+ /*if(document.getElementById("btnMen").onclick)
   {
     document.getElementById("list").style="display:block";
     document.getElementById("head").style="display:none";
     document.getElementById("boxNot").style="display:none";
     document.getElementById("clasf").setAttribute("class=sombra");
-  }
-
-
-}
+  }*/
